f844f320f355822455d4b83a184cbaef93bfbdde
Willian Alves <wba.alves@gmail.com>
2019-11-20 01:26:30 -0300

    update uploader to use boto3

--- s3.py
+++ s3.py
@@ -1,39 +1,21 @@
-from uuid import uuid4
-import boto
+import boto3
 import os.path
 from flask import current_app as app
 from werkzeug.utils import secure_filename
 
 
-def s3_upload(source_file, upload_dir=None, acl='public-read'):
-    """ Uploads WTForm File Object to Amazon S3
-
-        Expects following app.config attributes to be set:
-            AWS_KEY             :   S3 API Key
-            AWS_SECRET          :   S3 Secret Key
-            S3_BUCKET           :   What bucket to upload to
-            S3_UPLOAD_DIRECTORY :   Which S3 Directory.
-
-        The default sets the access rights on the uploaded file to
-        public-read.  It also generates a unique filename via
-        the uuid4 function combined with the file extension from
-        the source file.
-    """
-
-    if upload_dir is None:
-        upload_dir = app.config["S3_UPLOAD_DIRECTORY"]
-
+def s3_upload(source_file):
     source_filename = secure_filename(source_file.data.filename)
-    source_extension = os.path.splitext(source_filename)[1]
-
-    destination_filename = uuid4().hex + source_extension
-
     # Connect to S3 and upload file.
-    conn = boto.connect_s3(app.config["AWS_KEY"], app.config["AWS_SECRET"])
-    b = conn.get_bucket(app.config["S3_BUCKET"])
-
-    sml = b.new_key("/".join([upload_dir, destination_filename]))
-    sml.set_contents_from_string(source_file.data.read())
-    sml.set_acl(acl)
-
-    return destination_filename
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=app.config["AWS_KEY"],
+        aws_secret_access_key=app.config["AWS_SECRET"],
+        aws_session_token=app.config["AWS_SESSION"]
+    )
+    s3.put_object(
+        Bucket=app.config["S3_BUCKET"],
+        Key=source_filename,
+        Body=source_file.data
+    )
+    return source_filename
