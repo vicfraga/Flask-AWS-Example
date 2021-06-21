d37e1fec40a23a21376b48d61f15df460a84c3df
vicfraga <43010443+vicfraga@users.noreply.github.com>
2021-06-21 20:50:19 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,4 +2,8 @@
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip
+sudo locale-gen pt_BR.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
 pip install -r requirements.txt
