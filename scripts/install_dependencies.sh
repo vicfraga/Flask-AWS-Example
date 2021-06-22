09bf6f60af0c116ca01912e5ca5cf60d3657d760
vicfraga <43010443+vicfraga@users.noreply.github.com>
2021-06-21 21:22:59 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,9 @@
 cd /home/ubuntu
 sudo apt-get update
 sudo apt-get install -y python2.7 python-pip
+mkdir venv
+virtualenv venv
+source venv/bin/activate
 sudo locale-gen pt_BR.UTF-8
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
